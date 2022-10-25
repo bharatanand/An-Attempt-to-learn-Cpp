4876dabeaccf9123ae40e9de0df831cd569e9a70
bharatanand <bharatanand@outlook.in>
2022-10-26 01:03:10 +0530

    well about that

---
+++ somthing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int i;
+    cout << "Enter your favorite number: between 1 and 100" << endl;
+    cin >> i;
+    cout <<"The number you want to select: between 1 and 100 is : "<< i << endl;
+    return 0;
+}
